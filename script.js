01ee5f3c3ca52feec25a0e4fae4c60bda6c655b1
ccrenon <113647365+ccrenon2@users.noreply.github.com>
2023-06-26 09:24:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-var element = document.getElementById(input1);
+function butotnClick(){
+  msg.innerText = nameText.value;
+}
+
+let link = document.getElementById('link');
+link.value = 'link';
+let url = document.getElementById('url');
+
+let checkButton = document.getElementById('checkButton');
+checkButton.addEventListener('click', butotnClick);
